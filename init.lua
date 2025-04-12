1a48ccf408497054abe1da5d59dff316a1df36d9
schnow265 <thesnowbox@icloud.com>
2025-04-12 17:42:37 +0200

    more random stuff

--- init.lua
+++ init.lua
@@ -1,5 +1,15 @@
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  require("os.win")
+elseif vim.loop.os_uname().sysname == "Linux" then
+  require("os.linux")
+end
+
 vim.g.mapleader = "<space>"
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
 vim.o.number = true
+
+if vim.g.neovide then
+  require("config.neovide")
+end
