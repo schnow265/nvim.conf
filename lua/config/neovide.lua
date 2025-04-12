e4e384718764f23747d5f9572472a905bd9e5cd8
schnow265 <thesnowbox@icloud.com>
2025-04-12 23:30:01 +0200

    neovide: disable all animations & allow clipboard pasting

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -4,3 +4,27 @@ vim.o.guifont = "SauceCodePro Nerd Font:h12"
 -- vim.g.neovide_cursor_vfx_mode = "railgun"
 vim.g.neovide_cursor_animation_length = 0.13
 vim.g.neovide_cursor_trail_size = 0.8
+
+vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
+-- Nuclear option: disable all animations.
+-- Gosh do I hate them.
+
+vim.g.neovide_position_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0.00
+vim.g.neovide_cursor_trail_size = 0
+vim.g.neovide_cursor_animate_in_insert_mode = false
+vim.g.neovide_cursor_animate_command_line = false
+vim.g.neovide_scroll_animation_far_lines = 0
+vim.g.neovide_scroll_animation_length = 0.00
