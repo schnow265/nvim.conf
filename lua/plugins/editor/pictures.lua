a0c79e1f21fa405016c39bda892a6b715d1858db
schnow265 <thesnowbox@icloud.com>
2025-04-13 14:35:01 +0200

    use a txt file of random quotes for the header

---
+++ lua/plugins/editor/pictures.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "kjuq/sixelview.nvim",
+    opts = {
+      -- a table to specify what files should be viewed by this plugin
+      pattern = {},
+      -- whether to show an image automatically when an image buffer is opened
+      auto = true,
+      -- time of delay before showing image
+      -- try setting this duration longer if you have a trouble showing image
+      delay_ms = 100,
+    },
+  },
+}
